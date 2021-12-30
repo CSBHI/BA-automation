dee1d2d895dad19ab5602db5f56f6bb8de695a1c
Abhishek kumar <abhishekkumarbr8@gmail.com>
2021-12-31 00:03:11 +0530

    my first commit

--- src/test/java/Pages/LoginPage.java
+++ src/test/java/Pages/LoginPage.java
@@ -1,18 +1,27 @@
 package Pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class LoginPage {
     public static void main(String[] args) throws InterruptedException {
         System.setProperty("webdriver.chrome.driver", "C:\\browserdriver\\chromedriver.exe");
-
-        String email, password;
-
-
         WebDriver driver = new ChromeDriver();
-        //search google
-        driver.get("https://www.google.com/");
+
+        String email,password;
+
+        driver.get("https://chemistatplay.com");
+        driver.manage().window().maximize();
+        driver.manage().deleteAllCookies();
+        Thread.sleep(2000);
+
+       driver.findElement(By.xpath("//*[@id=\"SiteHeader\"]/div[1]/div[1]/div/div[3]/div/div[1]/a[2]")).click();
+        driver.findElement(By.xpath("//*[@id=\"CustomerEmail\"]")).sendKeys("abhishek@onestolabs.com");
+        driver.findElement(By.xpath("//*[@id=\"CustomerPassword\"]")).sendKeys("abhishek@123");
+        driver.findElement(By.xpath("//*[@id=\"customer_login\"]/p[1]/button")).click();
+
 
     }
-}
+
+    }
