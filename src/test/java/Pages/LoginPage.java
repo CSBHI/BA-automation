aa4bb1d14b9a2a46a12d052a02434d9b6dc7b770
Abhishek kumar <abhishekkumarbr8@gmail.com>
2021-12-30 17:37:23 +0530

    my first commit

--- src/test/java/Pages/LoginPage.java
+++ src/test/java/Pages/LoginPage.java
@@ -1,5 +1,18 @@
 package Pages;
 
-public class LoginPage {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
+public class LoginPage {
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "C:\\browserdriver\\chromedriver.exe");
+
+        String email, password;
+
+
+        WebDriver driver = new ChromeDriver();
+        //search google
+        driver.get("https://www.google.com/");
+
+    }
 }
